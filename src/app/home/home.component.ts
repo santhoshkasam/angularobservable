03eb5aca6718277b12f4fee8608c7e5e554f6ff0
Sharada Kasam <sharadakasam@outlook.com>
2023-04-18 05:29:20 +0530

    custom observable with interval method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  //To access the observable so that we can destroy them in OnDestroy method
+  firstObsSubscription: Subscription
   constructor() { }
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 
   ngOnInit() {
+    //Every sec the event will be emitted
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
   }
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,6 +13,11 @@ export class UserComponent implements OnInit {
   }
 
   ngOnInit() {
+    /*Below setup, the subsrciption of changing route parameters.
+    Here is params is an observable which we are subscribing
+    Observable is stream of data when new data emitted our observable will know about it
+    Prams is an observable. Its a stream of route parameters and that stream gives us a new route parameter
+    whenever we goto a new page, whenever that parameter in the URL changes */
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
